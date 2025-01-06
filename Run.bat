0662592ce098c7f8de507d9b6465543e909ac088
Kien Vu <105603108+kiezu01@users.noreply.github.com>
2025-01-06 15:57:29 +0700

    Run all scripts

---
+++ Run.bat
@@ -0,0 +1,5 @@
+@echo off
+call conda activate base
+python "C:\Users\Dell\Desktop\Occasional Tasks\9. Update payment\Step1.py"
+python "C:\Users\Dell\Desktop\Occasional Tasks\9. Update payment\Step2.py"
+pause
